c62df0590dc0b6ac61290c24336fd47adb0816aa
rifxtt <dc.wang547@gmail.com>
2023-02-08 14:23:37 -0500

    tailwind react-router setup

---
+++ components/index.tsx
@@ -0,0 +1,9 @@
+export {default as Login} from '../route/login/Login'
+export {default as Register} from '../route/register/Register'
+export {default as Single} from '../route/post/Single'
+export {default as Write} from '../route/write/Write'
+export {default as Layout} from './Layout'
+export {default as Navbar} from './Navbar'
+export {default as Footer} from './Footer'
+
+
---
+++ src/main.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import {Login, Register, Single, Write, Layout} from '../components/index';
+import ErrorPage from '../route/error/Errorpage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '/',
+        element: <App/>
+      },
+      {
+        path: '/post/:id',
+        element: <Single/>
+      },
+      {
+        path: '/write',
+        element: <Write/>
+      }
+    ],
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+  <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
