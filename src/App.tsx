62f83d07942af3ea7b5e5c17eb011c44740e9527
rifxtt <dc.wang547@gmail.com>
2023-02-19 18:19:40 -0500

    homepage design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const App = () => {
+
+  // const [posts, setPost] = useState<Post []>([]);
+
+   const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ];
+
   return (
-    <h1 className='underline'>This is homepage</h1>
+    <div>
+      <div className='flex flex-col my-24 gap-40 items-center justify-center'>
+          {posts.map((post)=>
+            <div key={post.id} className='post flex gap-16 w-11/12 odd:flex-row-reverse'>
+              <div className='img w-9/12 relative z-10'>
+                <img src={post.img} alt='post image' className="w-full h-96"/>
+              </div>
+              <div className='content flex flex-col justify-between'>
+                <Link to={`/posts/${post.id}`} className='title font-bold text-4xl font-sans antialiased'>{post.title}</Link>
+                <p className='text-lg'>{post.desc}</p>
+                <button className= 'px-2 py-4 rounded bg-slate-200 w-40 text-sm font-sans hover:bg-slate-300 font-medium'>Read More</button>
+              </div>
+            </div>
+          )}
+        </div>
+    </div>
   )
 }
 
